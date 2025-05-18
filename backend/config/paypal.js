eadf056a4f857069d8a23329c766b71fbab11b26
AndyHi <1155176724@link.cuhk.edu.hk>
2025-05-18 17:05:48 +0800

    Delete backend/config/paypal.js

--- backend/config/paypal.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PAYPAL_MODE: 'sandbox',
-    CURRENCY: 'HKD',
-    PAYPAL_MERCHANT_EMAIL: 'sb-t4qwr40698380@business.example.com',
-    PAYPAL_SANDBOX_ACCOUNT: 'sb-t4qwr40698380@business.example.com'
-}; 
