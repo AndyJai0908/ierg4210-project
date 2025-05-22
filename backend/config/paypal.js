9cc5e4bb096bc4e45389e21cd52ad01fd4ab2450
AndyHi <1155176724@link.cuhk.edu.hk>
2025-05-23 03:11:07 +0800

    Delete backend/config/paypal.js

--- backend/config/paypal.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AUZFLH0TT1DI_kxI4k135T_aIKX9ifIessJkPNzlLKcN8qBADatNAre1V1MsOWpsKR5RmyWueFSZa5uZ',
-    PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET || 'EBEdqwbibaE4cp1LtGrECkQuBHALECIn-7Fw_h8MvUSPjKNHq5Oyu48tHipEIRvUCxe4ALBijoNDCW6n',
-    PAYPAL_MODE: 'sandbox',
-    CURRENCY: 'HKD',
-    PAYPAL_MERCHANT_EMAIL: 'sb-t4qwr40698380@business.example.com',
-    PAYPAL_SANDBOX_ACCOUNT: 'sb-t4qwr40698380@business.example.com'
-}; 
